f39f4239c9054f7cb4c08f03f2016d4d3b6cbb94
unknown <langswd@W5095618.uk.experian.local>
2014-02-11 14:40:15 +0000

    revert

--- ffiii_job_roll.py
+++ ffiii_job_roll.py
@@ -10,7 +10,6 @@ class ff_job_roll(tkinter.Tk):
         self.initialise()
 
     # main menu. new game, load progress
-<<<<<<< HEAD
     # new game: FF1, FF3 NES, FF3 iOS, FF5 (any/melee/magic), FF12, BD!
     def initialise(self):
 
@@ -23,18 +22,6 @@ class ff_job_roll(tkinter.Tk):
                 windStatsLabel,
                 summaryText
             ):
-=======
-    # new game: FF1, FF3 NES, FF3 iOS, FF5 (any/melee/magic), FF12, BD
-    def initialise(self):
-
-        def start_game(self, game, 
-                       new_game_frame, ff3new_frame):
-            
-            def OnFF3WindButtonClick(
-                    windButton, fireButton,
-                    windLabel, windPicLabel,
-                    windStatsLabel, summaryText):
->>>>>>> Ogreman-master
                 windButton['state'] = 'disabled'
                 windButton['text'] = 'Wind Crystal Complete!'
                 fireButton['state'] = 'normal'
@@ -46,7 +33,6 @@ class ff_job_roll(tkinter.Tk):
                 summaryText.set("Defeat Salamander to unlock fire jobs...")
 
             def OnFF3FireButtonClick(
-<<<<<<< HEAD
                 fireButton,
                 waterButton,
                 fireLabel,
@@ -54,11 +40,6 @@ class ff_job_roll(tkinter.Tk):
                 fireStatsLabel,
                 summaryText
             ):
-=======
-                    fireButton, waterButton, 
-                    fireLabel, firePicLabel,
-                    fireStatsLabel, summaryText):
->>>>>>> Ogreman-master
                 fireButton['state'] = 'disabled'
                 fireButton['text'] = 'Fire Crystal Complete!'
                 waterButton['state'] = 'normal'
@@ -70,7 +51,6 @@ class ff_job_roll(tkinter.Tk):
                 summaryText.set("Defeat Salamander to unlock water jobs...")
 
             def OnFF3WaterButtonClick(
-<<<<<<< HEAD
                 waterButton,
                 earthButton,
                 waterLabel,
@@ -78,11 +58,6 @@ class ff_job_roll(tkinter.Tk):
                 waterStatsLabel,
                 summaryText
             ):
-=======
-                    waterButton, earthButton,
-                    waterLabel, waterPicLabel,
-                    waterStatsLabel, summaryText):
->>>>>>> Ogreman-master
                 waterButton['state'] = 'disabled'
                 waterButton['text'] = 'Water Crystal Complete!'
                 earthButton['state'] = 'normal'
@@ -94,17 +69,12 @@ class ff_job_roll(tkinter.Tk):
                 summaryText.set("Defeat Salamander to unlock earth jobs...")
 
             def OnFF3EarthButtonClick(
-<<<<<<< HEAD
                 earthButton,
                 earthLabel,
                 earthPicLabel,
                 earthStatsLabel,
                 summaryText
             ):
-=======
-                    earthButton, earthLabel, earthPicLabel, 
-                    earthStatsLabel, summaryText):
->>>>>>> Ogreman-master
                 earthButton['state'] = 'disabled'
                 earthButton['text'] = 'Earth Crystal Complete!'
 
@@ -122,23 +92,14 @@ class ff_job_roll(tkinter.Tk):
 
             #ff3new_frame.destroy()
             ff3new_frame.pack()
-<<<<<<< HEAD
 
 
-=======
-            
-            
->>>>>>> Ogreman-master
             '''counter = 0
             for f in self.winfo_children():
                 counter += 1
 
             if counter == 2:
-<<<<<<< HEAD
 
-=======
-                
->>>>>>> Ogreman-master
                 ff3new_frame.pack()
             else:
                 print("destroying")
@@ -146,20 +107,12 @@ class ff_job_roll(tkinter.Tk):
                 ff3new_frame = tkinter.Frame(self)
                 ff3new_frame.pack()'''
 
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> Ogreman-master
             ff3new_frame.configure(bg="white")
             #self.configure(background='white')
             #self.geometry("800x430")
             labelFont=('Helvetica', 24)
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> Ogreman-master
             windLabelVariable = tkinter.StringVar()
             fireLabelVariable = tkinter.StringVar()
             waterLabelVariable = tkinter.StringVar()
@@ -178,15 +131,9 @@ class ff_job_roll(tkinter.Tk):
             windLabelVariable.set(windJobText)
 
             #
-<<<<<<< HEAD
             windStatsText.set("STR: " + windStats[0] +
                               "\nAGL: " + windStats[1] +
                               "\nVIT: " + windStats[2] +
-=======
-            windStatsText.set("STR: " + windStats[0] + 
-                              "\nAGL: " + windStats[1] + 
-                              "\nVIT: " + windStats[2] + 
->>>>>>> Ogreman-master
                               "\nINT: " + windStats[3] +
                               "\nMND: " + windStats[4])
 
@@ -197,15 +144,9 @@ class ff_job_roll(tkinter.Tk):
             fireLabelVariable.set(fireJobText)
 
             #
-<<<<<<< HEAD
             fireStatsText.set("STR: " + fireStats[0] +
                               "\nAGL: " + fireStats[1] +
                               "\nVIT: " + fireStats[2] +
-=======
-            fireStatsText.set("STR: " + fireStats[0] + 
-                              "\nAGL: " + fireStats[1] + 
-                              "\nVIT: " + fireStats[2] + 
->>>>>>> Ogreman-master
                               "\nINT: " + fireStats[3] +
                               "\nMND: " + fireStats[4])
 
@@ -216,15 +157,9 @@ class ff_job_roll(tkinter.Tk):
             waterLabelVariable.set(waterJobText)
 
             #
-<<<<<<< HEAD
             waterStatsText.set("STR: " + waterStats[0] +
                                "\nAGL: " + waterStats[1] +
                                "\nVIT: " + waterStats[2] +
-=======
-            waterStatsText.set("STR: " + waterStats[0] + 
-                               "\nAGL: " + waterStats[1] + 
-                               "\nVIT: " + waterStats[2] + 
->>>>>>> Ogreman-master
                                "\nINT: " + waterStats[3] +
                                "\nMND: " + waterStats[4])
 
@@ -235,7 +170,6 @@ class ff_job_roll(tkinter.Tk):
             earthLabelVariable.set(earthJobText)
 
             #
-<<<<<<< HEAD
             earthStatsText.set("STR: " + earthStats[0] +
                 "\nAGL: " + earthStats[1] +
                 "\nVIT: " + earthStats[2] +
@@ -266,26 +200,6 @@ class ff_job_roll(tkinter.Tk):
                 ff3new_frame,
                 textvariable=earthStatsText
             )
-=======
-            earthStatsText.set("STR: " + earthStats[0] + 
-                               "\nAGL: " + earthStats[1] + 
-                               "\nVIT: " + earthStats[2] + 
-                               "\nINT: " + earthStats[3] +
-                               "\nMND: " + earthStats[4])
-
-            #
-            summaryLabel = tkinter.Label(ff3new_frame, 
-                                         textvariable=summaryText,
-                                         anchor="w",fg="white",bg="black")
-            windStatsLabel = tkinter.Label(ff3new_frame, 
-                                           textvariable=windStatsText)
-            fireStatsLabel = tkinter.Label(ff3new_frame,
-                                           textvariable=fireStatsText)
-            waterStatsLabel = tkinter.Label(ff3new_frame,
-                                            textvariable=waterStatsText)
-            earthStatsLabel = tkinter.Label(ff3new_frame,
-                                            textvariable=earthStatsText)
->>>>>>> Ogreman-master
 
             #
             windImage = ImageTk.PhotoImage(Image.open(windJobPath))
@@ -294,7 +208,6 @@ class ff_job_roll(tkinter.Tk):
             earthImage = ImageTk.PhotoImage(Image.open(earthJobPath))
 
             #
-<<<<<<< HEAD
             windLabel = tkinter.Label(
                 ff3new_frame,
                 textvariable=windLabelVariable
@@ -311,16 +224,6 @@ class ff_job_roll(tkinter.Tk):
                 ff3new_frame,
                 textvariable=earthLabelVariable
             )
-=======
-            windLabel = tkinter.Label(ff3new_frame,
-                                      textvariable=windLabelVariable)
-            fireLabel = tkinter.Label(ff3new_frame,
-                                      textvariable=fireLabelVariable)           
-            waterLabel = tkinter.Label(ff3new_frame,
-                                       textvariable=waterLabelVariable)
-            earthLabel = tkinter.Label(ff3new_frame,
-                                       textvariable=earthLabelVariable)
->>>>>>> Ogreman-master
 
             #
             windPicLabel = tkinter.Label(ff3new_frame, image=windImage)
@@ -334,7 +237,6 @@ class ff_job_roll(tkinter.Tk):
 
             #windPicLabel.grid(column=0,row=3,columnspan=1,sticky='WE')
             #
-<<<<<<< HEAD
             windButton = tkinter.Button(
                 ff3new_frame,
                 text=u"Wind Crystal Get!",
@@ -355,27 +257,13 @@ class ff_job_roll(tkinter.Tk):
                 text=u"Earth Crystal Get!",
                 state='disabled'
             )
-=======
-            windButton = tkinter.Button(ff3new_frame,text=u"Wind Crystal Get!", 
-                                        state='normal')
-            fireButton = tkinter.Button(ff3new_frame,text=u"Fire Crystal Get!", 
-                                        state='disabled')
-            waterButton = tkinter.Button(ff3new_frame,text=u"Water Crystal Get!", 
-                                         state='disabled')
-            earthButton = tkinter.Button(ff3new_frame,text=u"Earth Crystal Get!", 
-                                         state='disabled')
->>>>>>> Ogreman-master
 
             #
             windStatsLabel.config(anchor="center",font=statsFont,bg='white')
             fireStatsLabel.config(anchor="center",font=statsFont,bg='white')
             waterStatsLabel.config(anchor="center",font=statsFont,bg='white')
             earthStatsLabel.config(anchor="center",font=statsFont,bg='white')
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> Ogreman-master
             #
             summaryLabel.grid(column=0,row=2,columnspan=4,sticky='EW')
             summaryLabel.config(height=2,anchor='center')
@@ -395,7 +283,6 @@ class ff_job_roll(tkinter.Tk):
 
             #
             windButton.config(
-<<<<<<< HEAD
                 height=2,
                 command=lambda: OnFF3WindButtonClick(
                     windButton,
@@ -438,39 +325,6 @@ class ff_job_roll(tkinter.Tk):
                     summaryText
                 )
             )
-=======
-                    height=2,
-                    command=lambda: OnFF3WindButtonClick(
-                            windButton,
-                            fireButton,
-                            windLabel,
-                            windPicLabel,
-                            windStatsLabel,
-                            summaryText
-                    )
-            )
-            fireButton.config(
-                    height=2,
-                    command=lambda: OnFF3FireButtonClick(
-                            fireButton, 
-                            waterButton, 
-                            fireLabel,
-                            firePicLabel,
-                            fireStatsLabel,
-                            summaryText
-                    )
-            )
-            waterButton.config(
-                    height=2,
-                    command=lambda: OnFF3WaterButtonClick(
-                            waterButton, 
-                            earthButton, 
-                            waterLabel, 
-                            waterPicLabel, 
-                            waterStatsLabel, 
-                            summaryText))
-            earthButton.config(height=2, command=lambda: OnFF3EarthButtonClick(earthButton, earthLabel, earthPicLabel, earthStatsLabel, summaryText))
->>>>>>> Ogreman-master
 
             # Stretches components in column 0 with window resize
             ff3new_frame.grid_columnconfigure(0,minsize=200,weight=1)
@@ -484,7 +338,6 @@ class ff_job_roll(tkinter.Tk):
         def ff1():
             print("ff1")
 
-<<<<<<< HEAD
         def ff3nes():
             start_game("ff3n")
 
@@ -498,13 +351,6 @@ class ff_job_roll(tkinter.Tk):
                     new_game_frame,
                     ff3new_frame
             )
-=======
-        def ff3nes(new_game_frame, ff3new_frame):
-            start_game(self, "ff3n", new_game_frame, ff3new_frame)
-
-        def ff3new(new_game_frame, ff3new_frame):
-            start_game(self, "ff3r", new_game_frame, ff3new_frame)
->>>>>>> Ogreman-master
 
         def ff5():
             print("ff5")
@@ -521,7 +367,6 @@ class ff_job_roll(tkinter.Tk):
 
         newGameMenu = tkinter.Menu(menuBar, tearoff=0)
         newGameMenu.add_command(label="Final Fantasy I", command=ff1)
-<<<<<<< HEAD
         newGameMenu.add_command(label="Final Fantasy III (NES)", command=ff3nes)
         newGameMenu.add_command(
             label="Final Fantasy III (Remake)",
@@ -535,12 +380,6 @@ class ff_job_roll(tkinter.Tk):
             label="Final Fantasy XII International",
             command=ff12
         )
-=======
-        newGameMenu.add_command(label="Final Fantasy III (NES)", command=lambda: ff3nes(new_game_frame, ff3new_frame))
-        newGameMenu.add_command(label="Final Fantasy III (Remake)", command=lambda: ff3new(new_game_frame, ff3new_frame))
-        newGameMenu.add_command(label="Final Fantasy V", command=ff5)
-        newGameMenu.add_command(label="Final Fantasy XII International", command=ff12)
->>>>>>> Ogreman-master
         newGameMenu.add_command(label="Bravely Default", command=bravelyd)
         menuBar.add_cascade(label="New Game", menu=newGameMenu)
         self.config(menu=menuBar)
@@ -548,7 +387,6 @@ class ff_job_roll(tkinter.Tk):
         new_game_frame = tkinter.Frame(self)
         ff3new_frame = tkinter.Frame(self)
         new_game_frame.configure(bg="white")
-<<<<<<< HEAD
         ff3rButton = tkinter.Button(
             new_game_frame,
             text=u"Final Fantasy III!",
@@ -561,11 +399,6 @@ class ff_job_roll(tkinter.Tk):
                 ff3new_frame
             )
         )
-=======
-        ff3rButton = tkinter.Button(new_game_frame,text=u"Final Fantasy III!",
-                                    state='normal')
-        ff3rButton.config(height=2, command=lambda: ff3new(new_game_frame, ff3new_frame))
->>>>>>> Ogreman-master
         ff3rButton.grid(column=0,row=0,sticky='EW')
         new_game_frame.pack()
 
